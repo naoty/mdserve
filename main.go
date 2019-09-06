e4d585229bbd5dcb0ae71ac60c760e452e59b17b
Naoto Kaneko <naoty.k@gmail.com>
2019-09-06 21:53:52 +0900

    Add a handler for /contents

--- main.go
+++ main.go
@@ -20,5 +20,6 @@ func main() {
 		}
 	}
 
-	http.ListenAndServe(":8000", &server.Server{})
+	server := server.New()
+	http.ListenAndServe(":8000", server)
 }
--- server/server.go
+++ server/server.go
@@ -6,8 +6,36 @@ import (
 
 // Server is a web server providing RESTful API for markdown contents.
 type Server struct {
+	routes map[string]http.Handler
+}
+
+// New returns a new Server.
+func New() *Server {
+	routes := map[string]http.Handler{}
+	routes["/contents"] = contentsHandler()
+
+	return &Server{
+		routes: routes,
+	}
+}
+
+func contentsHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+	})
 }
 
 func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusNotFound)
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	handler, ok := s.routes[r.URL.Path]
+	if !ok {
+		w.WriteHeader(http.StatusNotFound)
+		return
+	}
+
+	handler.ServeHTTP(w, r)
 }
