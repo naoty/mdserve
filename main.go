5b822df20dd61ea4ac89f6aefe8592bfe90e9801
Naoto Kaneko <naoty.k@gmail.com>
2019-09-08 14:39:11 +0900

    Parse markdown contents at passed directory

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"path/filepath"
 
 	"github.com/naoty/mdserve/contents"
 	"github.com/naoty/mdserve/server"
@@ -14,6 +15,7 @@ import (
 var Version = ""
 
 var help = `Usage:
+  mdserve <dir>
   mdserve -h | --help
   mdserve -v | --version
 
@@ -22,7 +24,13 @@ Options:
   -v --version  Show version.`
 
 func main() {
-	for _, arg := range os.Args {
+	if len(os.Args) == 1 {
+		fmt.Println(help)
+		os.Exit(1)
+	}
+
+	dir := ""
+	for _, arg := range os.Args[1:] {
 		switch arg {
 		case "-h", "--help":
 			fmt.Println(help)
@@ -30,22 +38,51 @@ func main() {
 		case "-v", "--version":
 			fmt.Println(Version)
 			os.Exit(0)
+		default:
+			info, err := os.Stat(arg)
+			if err != nil {
+				log.Fatalln(err)
+			}
+
+			if !info.IsDir() {
+				fmt.Println(help)
+				os.Exit(1)
+			}
+
+			dir = arg
 		}
 	}
 
-	file, err := os.Open("examples/post.md")
+	err := filepath.Walk(dir, parse)
 	if err != nil {
-		log.Fatalln(err)
-	}
-	defer file.Close()
-
-	err = contents.Parse(file)
-	if err != nil {
-		file.Close()
 		log.Fatalln(err)
 	}
 
 	logger := log.New(os.Stdout, "", 0)
 	server := server.New().WithLogger(logger)
 	http.ListenAndServe(":8000", server)
+}
+
+func parse(path string, info os.FileInfo, err error) error {
+	if err != nil {
+		return err
+	}
+
+	if info.IsDir() || filepath.Ext(path) != ".md" {
+		return nil
+	}
+
+	file, err := os.Open(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	err = contents.Parse(file)
+	if err != nil {
+		file.Close()
+		return err
+	}
+
+	return nil
 }
