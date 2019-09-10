d7062d491ed4e3be519040ce4859c696c074e6b0
Naoto Kaneko <naoty.k@gmail.com>
2019-09-11 00:02:39 +0900

    Add -H and --host flag to specify hostname

--- main.go
+++ main.go
@@ -16,16 +16,18 @@ import (
 var Version = ""
 
 var help = `Usage:
-  mdserve <dir> [-p | --port <port>]
+  mdserve <dir> [-H | --host <hostname>] [-p | --port <port>]
   mdserve -h | --help
   mdserve -v | --version
 
 Options:
-  -p --port <port>  Run web server at specified port [default: 8000].
-  -h --help         Show this message.
-  -v --version      Show version.`
+  -H --host <hostname> Run web server at specified hostname [default: localhost].
+  -p --port <port>     Run web server at specified port [default: 8000].
+  -h --help            Show this message.
+  -v --version         Show version.`
 
 func main() {
+	host := pflag.StringP("host", "H", "localhost", "")
 	port := pflag.IntP("port", "p", 8000, "")
 	helpFlag := pflag.BoolP("help", "h", false, "")
 	versionFlag := pflag.BoolP("version", "v", false, "")
@@ -64,7 +66,7 @@ func main() {
 	logger := log.New(os.Stdout, "", 0)
 	server := server.New(dir).WithLogger(logger)
 
-	addr := fmt.Sprintf(":%d", *port)
+	addr := fmt.Sprintf("%s:%d", *host, *port)
 	err = http.ListenAndServe(addr, server)
 	if err != nil {
 		log.Fatalln(err)
